dfbac6cd4ab39a9097304d20f910e29946faf00e
Brian Whims <bcwhims@gmail.com>
2018-04-20 22:28:55 -0400

    Removing unused fields.

--- ReflectionCacher/Extensions/ObjectExtensions.cs
+++ ReflectionCacher/Extensions/ObjectExtensions.cs
@@ -9,10 +9,6 @@ namespace ReflectionCacher.Extensions
 {
     public static class ObjectExtensions
     {
-        private static readonly ConcurrentDictionary<Type, TypeConverter> Converters = new ConcurrentDictionary<Type, TypeConverter>();
-        private static readonly ConcurrentDictionary<Type, object> Defaults = new ConcurrentDictionary<Type, object>();
-        private static readonly Type TypeHelperType = typeof(Types<>);
-
         public static T Convert<T>(this object obj)
         {
             return (T)Convert(obj, Types<T>.ConverterInstance, Types<T>.Type, Types<T>.DefaultValue);
@@ -28,7 +24,6 @@ namespace ReflectionCacher.Extensions
             {
                 return Types<T>.DefaultValue;
             }
-
         }
 
         public static object Convert(this object obj, Type type)
